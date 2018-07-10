9aecad8761f8fc0798248178cc6ebb1623a2412a
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-07-10 14:22:13 +0200

    Handle In-Website Preview by Prismic

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const prismicJS = require('prismic-javascript');
+const Cookies = require('cookies');
+
+const linkResolver = require('../link-resolver');
+
 const router = express.Router();
 
 /* GET homepage. */
@@ -9,6 +14,17 @@ router.get('/', function (req, res, next) {
     })
     .catch((error) => {
       next(`Error when retrieving "homepage" document from Prismic. ${error.message}`);
+    });
+});
+
+/* In-Website Preview by Prismic. */
+router.get('/preview', function (req, res) {
+  req.prismic.api.previewSession(req.query.token, linkResolver, '/')
+    .then((url) => {
+      const cookies = new Cookies(req, res);
+
+      cookies.set(prismicJS.previewCookie, req.query.token, { maxAge: 30 * 60 * 1000, path: '/', httpOnly: false });
+      res.redirect(302, url);
     });
 });
 
