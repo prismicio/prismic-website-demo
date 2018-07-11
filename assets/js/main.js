6bafd33f7a549ca424ab7d14ceb28f348bf9e720
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-07-11 18:48:13 +0200

    Do header responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 import "../scss/main.scss";
+
+$(document).ready(function () {
+  const $header = $('#header');
+
+  $('#header-burger').on('click', function () {
+    $header.toggleClass('header--is-nav-opened');
+  });
+});
